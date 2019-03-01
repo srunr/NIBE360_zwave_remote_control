ac2b431e85808a3d5c6a37d6bf3be3155bbac716
Sören Rundgren <48016758+srunr@users.noreply.github.com>
2019-03-01 20:12:14 +0100

    Add files via upload

---
+++ UNO_to_drive_relay_from_ZUNO_R4/UNO_to_drive_relay_from_ZUNO_R4.ino
@@ -0,0 +1,94 @@
+// Input pullup connected to the z-uno controller outputs to enable 
+// conversion from 3.3V to 5V to drive the VMA436 board.
+//Digital input PINS
+int IN1 = 1;
+int IN2 = 2;
+int IN3 = 3;
+int IN4 = 4;
+int IN5 = 5;
+
+// the digital output PINS
+int OUT1 = 8;
+int OUT2 = 9;
+int OUT3 = 10;
+int OUT4 = 11;
+int OUT5 = 12;
+
+byte readIN1 = HIGH;
+byte currIN1 = HIGH;
+byte readIN2 = HIGH;
+byte currIN2 = HIGH;
+byte readIN3 = HIGH;
+byte currIN3 = HIGH;
+byte readIN4 = HIGH;
+byte currIN4 = HIGH;
+byte readIN5 = HIGH;
+byte currIN5 = HIGH;
+
+void setup() 
+{
+  pinMode(IN1, INPUT_PULLUP); 
+  pinMode(IN2, INPUT_PULLUP); 
+  pinMode(IN3, INPUT_PULLUP); 
+  pinMode(IN4, INPUT_PULLUP); 
+  pinMode(IN5, INPUT_PULLUP); 
+
+  pinMode(OUT1, OUTPUT); 
+  pinMode(OUT2, OUTPUT); 
+  pinMode(OUT3, OUTPUT); 
+  pinMode(OUT4, OUTPUT); 
+  pinMode(OUT5, OUTPUT); 
+
+  // digitalWrite(OUT1,HIGH); // low = relay ON, high = relay OFF for velleman VMA436
+  // digitalWrite(OUT2,HIGH); // Do not need to initialize !!!
+  // digitalWrite(OUT3,HIGH); 
+  // digitalWrite(OUT4,HIGH); 
+  // digitalWrite(OUT5,HIGH); 
+  delay(100);
+}
+
+void loop()
+{
+  delay(100);
+  readIN1 = digitalRead(IN1);
+  if (readIN1 != currIN1)
+  {
+    digitalWrite(OUT1,!readIN1); 
+    currIN1 = readIN1;
+    delay(10);
+  }
+  readIN2 = digitalRead(IN2);
+  if(readIN2 != currIN2)
+  {
+    digitalWrite(OUT2,!readIN2); 
+    currIN2 = readIN2;
+    delay(10);
+  }
+  readIN3 = digitalRead(IN3);
+  if(readIN3 != currIN3)
+  {
+    digitalWrite(OUT3,!readIN3); 
+    currIN3 = readIN3;
+    delay(10);
+  }
+  readIN4 = digitalRead(IN4);
+  if(readIN4 != currIN4)
+  {
+    digitalWrite(OUT4,!readIN4); 
+    currIN4 = readIN4;
+    delay(10);
+  }
+  readIN5 = digitalRead(IN5);
+  if(readIN5 != currIN5)
+  {
+    digitalWrite(OUT5,!readIN5); 
+    currIN5 = readIN5;
+    delay(10);
+  }
+ 
+}
+
+
+
+
+
---
+++ zuno_nibe360_R5/zuno_nibe360_R5.ino
@@ -0,0 +1,132 @@
+
+/* 
+ * This scretch was certified by the Z-Wave Alliance as one of the two reference Z-Uno sketches.
+ * Modified for remote management of NIBE360 and using Arduino UNO as the shield for the relay board VMA 436
+ * Program the UNO using digital input pull up pins 1 - 5 and drive output pins 8 - 12 with active LOW.
+ * 5 relays switches R1-R5
+ * 2 alarm digital inputs I1-I2
+ */
+
+// Pins definitions
+#define R1_TARIFF_A_PIN 9
+#define R2_TARIFF_B_PIN 10
+#define R3_SHUNT_PLUS5_PIN 11
+#define I1_HP_ALARM_PIN 12
+#define I2_LP_ALARM_PIN 19
+#define R4_EXTRA_WARM_WATER_PIN 20
+#define R5_FAN_SPEED_II_PIN 21
+
+// Global variables to store data reported via getters
+byte switchTARIFF_A_VAL = 0;
+byte switchTARIFF_B_VAL = 0;
+byte switchSHUNT_PLUS5_VAL = 0;
+byte switchEXTRA_WARM_WATER_VAL = 0;
+byte switchFAN_SPEED_II_VAL = 0;
+byte lastHP_ALARM_VAL = 0;
+byte lastLP_ALARM_VAL = 0;
+
+ZUNO_SETUP_SLEEPING_MODE(ZUNO_SLEEPING_MODE_ALWAYS_AWAKE);
+
+ZUNO_SETUP_ASSOCIATIONS(ZUNO_ASSOCIATION_GROUP_SET_VALUE); // Send Basic Set to association group
+
+// Set up 7 channels
+ZUNO_SETUP_CHANNELS(
+  ZUNO_SWITCH_BINARY(getterR1, setterR1),
+  ZUNO_SWITCH_BINARY(getterR2, setterR2),
+  ZUNO_SWITCH_BINARY(getterR3, setterR3),
+  ZUNO_SWITCH_BINARY(getterR4, setterR4),
+  ZUNO_SWITCH_BINARY(getterR5, setterR5),
+  ZUNO_SENSOR_BINARY(ZUNO_SENSOR_BINARY_TYPE_GENERAL_PURPOSE, getterI1),
+  ZUNO_SENSOR_BINARY(ZUNO_SENSOR_BINARY_TYPE_GENERAL_PURPOSE, getterI2)
+);
+
+void setup() {
+  // set up I/O pins. 
+  pinMode(R1_TARIFF_A_PIN, OUTPUT);
+  digitalWrite(R1_TARIFF_A_PIN, HIGH); // Init High as the relay board is active LOW
+  pinMode(R2_TARIFF_B_PIN, OUTPUT);
+  digitalWrite(R2_TARIFF_B_PIN, HIGH); // Init High as the relay board is active LOW
+  pinMode(R3_SHUNT_PLUS5_PIN, OUTPUT);
+  digitalWrite(R3_SHUNT_PLUS5_PIN, HIGH); // Init High as the relay board is active LOW
+  pinMode(R4_EXTRA_WARM_WATER_PIN, OUTPUT);
+  digitalWrite(R4_EXTRA_WARM_WATER_PIN, HIGH); // Init High as the relay board is active LOW
+  pinMode(R5_FAN_SPEED_II_PIN, OUTPUT); 
+  digitalWrite(R5_FAN_SPEED_II_PIN, HIGH); // Init High as the relay board is active LOW
+  pinMode(I1_HP_ALARM_PIN, INPUT_PULLUP);
+  pinMode(I2_LP_ALARM_PIN, INPUT_PULLUP);
+  delay(1000); //delay 1 s
+}
+
+// Getters and setters
+
+void setterR1(byte value) {
+  digitalWrite(R1_TARIFF_A_PIN, (value > 0) ?  LOW : HIGH);
+  switchTARIFF_A_VAL = value;
+}
+
+byte getterR1(){
+  return switchTARIFF_A_VAL;
+}
+
+ void setterR2(byte value) {
+  digitalWrite(R2_TARIFF_B_PIN, (value > 0) ? LOW : HIGH);
+  switchTARIFF_B_VAL = value;
+}
+
+byte getterR2(){
+  return switchTARIFF_B_VAL;
+}
+
+void setterR3(byte value) {
+  digitalWrite(R3_SHUNT_PLUS5_PIN, (value > 0) ? LOW : HIGH);
+  switchSHUNT_PLUS5_VAL = value;
+}
+
+byte getterR3(){
+  return switchSHUNT_PLUS5_VAL;
+}
+void setterR4(byte value) {
+  digitalWrite(R4_EXTRA_WARM_WATER_PIN, (value > 0) ? LOW : HIGH);
+  switchEXTRA_WARM_WATER_VAL = value;
+}
+
+byte getterR4(){
+  return switchEXTRA_WARM_WATER_VAL;
+}
+void setterR5(byte value) {
+  digitalWrite(R5_FAN_SPEED_II_PIN, (value > 0) ? LOW : HIGH);
+  switchFAN_SPEED_II_VAL = value;
+}
+
+byte getterR5(){
+  return switchFAN_SPEED_II_VAL;
+}
+
+byte getterI1(void) {
+  return lastHP_ALARM_VAL ? 0xff : 0;
+}
+
+byte getterI2(void) {
+  return lastLP_ALARM_VAL ? 0xff : 0;
+}
+
+void loop() {
+  byte current_HP_ALARM_VAL;
+  byte current_LP_ALARM_VAL;
+
+  // NIBE360 Alarms
+  current_HP_ALARM_VAL = digitalRead(I1_HP_ALARM_PIN); 
+  if (current_HP_ALARM_VAL != lastHP_ALARM_VAL) { 
+    lastHP_ALARM_VAL = current_HP_ALARM_VAL;
+    zunoSendReport(6);
+  }
+  
+  current_LP_ALARM_VAL = digitalRead(I2_LP_ALARM_PIN); 
+  if (current_LP_ALARM_VAL != lastLP_ALARM_VAL) { 
+    lastLP_ALARM_VAL = current_LP_ALARM_VAL;
+    zunoSendReport(7);
+  }
+  delay(100);
+}
+
+
